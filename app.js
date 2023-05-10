4a279d27d7fa0ee4379a5d767976c517a00b6ae4
Spsoi <you@example.com>
2023-05-10 20:38:22 +0300

    outputting lists v-for

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const app = Vue.createApp({
             author: 'Branden Sanderson',
             age: 45,
             x: 0,
-            y: 0
+            y: 0,
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss'},
+                {title: 'the way of kings', author: 'brandon sanderson'},
+                {title: 'the final empire', author: 'brandon sanderson'},
+            ]
         }
     },
     methods: {
